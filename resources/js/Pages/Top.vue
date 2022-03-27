8237a6e48ebefbf161d361522bccb7c073189a0c
itazulala <minaton.tutan@gmail.com>
2022-03-27 18:47:37 +0900

    トップ画面作成

--- resources/js/Pages/Top.vue
+++ resources/js/Pages/Top.vue
@@ -2,54 +2,42 @@
     <v-app>
         <Header></Header>
         <Sidebar></Sidebar>
-        <v-main>
+        <v-main class="blue-grey lighten-5">
             <v-container class="pa-10">
-                <v-card class="pt-2 pl-5 pr-5">
-                    <v-card-title class="border-b-2">作品一覧</v-card-title>
-                    <v-card-actions class="ma-5">
-                        <v-spacer></v-spacer>
-                        <v-icon icon large class="mx-2">mdi-format-list-bulleted-square</v-icon>
-                        <v-icon icon large class="mx-2">mdi-view-grid-outline</v-icon>
-                        <v-icon icon large class="mx-2">mdi-grid</v-icon>
-                    </v-card-actions>
-                    <v-container fluid>
-                        <v-row>
-                            <v-col
-                                v-for="card in cards"
-                                :key="card.title"
-                                xs="12"
-                                sm="6"
-                                md="4"
-                            >
-                                <v-card flat>
-                                    <v-card-text>
-                                        <v-img
-                                            :src="card.src"
-                                            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                                            class="mx-auto my-0"
-
-                                        >
-                                        </v-img>
-                                    </v-card-text>
-                                    <v-card-actions class="pa-3">
-                                        {{ card.title }}
-                                    </v-card-actions>
-                                    <div class="dashed"></div>
-                                    <v-card-actions class="pa-3">
-                                        <v-spacer></v-spacer>
-                                        {{ card.creator }}
-                                    </v-card-actions>
-                                    <div class="dashed"></div>
-                                    <v-card-actions class="pa-3">
-                                        <v-btn>入札</v-btn>
-                                        <v-spacer></v-spacer>
-                                        <v-icon icon v-on:click="">mdi-heart-outline</v-icon>
-                                    </v-card-actions>
-                                    <div class="dashed"></div>
-                                </v-card>
-                            </v-col>
-                        </v-row>
-                    </v-container>
+                <v-card class="pt-5 pb-5 pl-10 pr-10">
+                    <v-card-title class="border-b-2 mb-5 text-subtitle-1">ランキング</v-card-title>
+                    <v-row>
+                        <v-col
+                            v-for="item in items"
+                            :key="item.img"
+                            cols="2"
+                        >
+                            <v-card-text>
+                                <v-img :src="item.img"></v-img>
+                            </v-card-text>
+                            <v-card-text class="dashed caption">作品名: {{ item.title }}</v-card-text>
+                            <v-card-text class="dashed caption">作者名: {{ item.artist }}</v-card-text>
+                        </v-col>
+                    </v-row>
+                    <v-card-text class="caption text-right"><a href="#">もっと見る</a></v-card-text>
+                </v-card>
+                <v-card
+                    v-for="feature in features"
+                    :key="feature.mainTitle"
+                    class="mt-10 pt-5 pb-5 pl-10 pr-10"
+                >
+                    <v-card-title class="border-b-2 mb-5 text-subtitle-1">{{ feature.mainTitle }}</v-card-title>
+                    <v-row>
+                        <v-col
+                            v-for="item in feature.items"
+                            :key="item.img"
+                            cols="2"
+                        >
+                            <v-card-text><v-img :src="item.img"></v-img></v-card-text>
+                            <v-card-text class="dashed caption">作品名: {{ item.title }}</v-card-text>
+                            <v-card-text class="dashed caption">作者名: {{ item.artist }}</v-card-text>
+                        </v-col>
+                    </v-row>
                 </v-card>
             </v-container>
         </v-main>
@@ -70,47 +58,41 @@ export default {
     },
     data() {
         return {
-            drawer: null,
-            selectedItem: 1,
             items: [
-                {text: '入札中', icon: 'mdi-gavel', link: '#'},
-                {text: '入金待ち', icon: 'mdi-currency-jpy', link: '#'},
-                {text: '受取待ち', icon: 'mdi-truck-delivery-outline', link: '#'},
+                {img: 'storage/images/amagasa_house.png', title: '雨笠の家', artist: '雨笠 将'},
+                {img: 'storage/images/amagasa_house.png', title: '雨笠の家', artist: '雨笠 将'},
+                {img: 'storage/images/amagasa_house.png', title: '雨笠の家', artist: '雨笠 将'},
+                {img: 'storage/images/amagasa_house.png', title: '雨笠の家', artist: '雨笠 将'},
+                {img: 'storage/images/amagasa_house.png', title: '雨笠の家', artist: '雨笠 将'},
+                {img: 'storage/images/amagasa_house.png', title: '雨笠の家', artist: '雨笠 将'},
+                {img: 'storage/images/amagasa_house.png', title: '雨笠の家', artist: '雨笠 将'},
+                {img: 'storage/images/amagasa_house.png', title: '雨笠の家', artist: '雨笠 将'},
+                {img: 'storage/images/amagasa_house.png', title: '雨笠の家', artist: '雨笠 将'},
+                {img: 'storage/images/amagasa_house.png', title: '雨笠の家', artist: '雨笠 将'},
             ],
-            kinds: [
-                ['絵画', ""],
-                ['日本画', ""],
-                ['油絵', ""],
-                ['水彩画', ""],
-                ['ペン画', ""],
-                ['イラスト', ""],
-                ['ポスター', ""],
-                ['ポストカード', ""],
-                ['版画', ""]
-            ],
-            colors: [
-                ['赤', ""],
-                ['青', ""],
-                ['黄色', ""],
-                ['ピンク', ""],
-                ['白', ""],
-                ['黒', ""],
-                ['緑', ""],
-                ['モノクロ', ""],
-                ['カラフル', ""],
-                ['パステル', ""]
-            ],
-            sises: [
-                ['用紙サイズ', ""],
-                ['', ""],
-            ],
-            cards: [
-                {title: 'お家', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', creator: '渡邉 優太'},
-                {title: 'お家', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', creator: '渡邉 優太'},
-                {title: 'お家', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', creator: '渡邉 優太'},
-                {title: 'お家', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', creator: '渡邉 優太'},
-                {title: 'お家', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', creator: '渡邉 優太'},
-
+            features: [
+                {
+                    mainTitle: '特集1',
+                    items: [
+                        {img: 'storage/images/amagasa_zange.png', title: '雨笠の懺悔', artist: '雨笠 将'},
+                        {img: 'storage/images/amagasa_zange.png', title: '雨笠の懺悔', artist: '雨笠 将'},
+                        {img: 'storage/images/amagasa_zange.png', title: '雨笠の懺悔', artist: '雨笠 将'},
+                        {img: 'storage/images/amagasa_zange.png', title: '雨笠の懺悔', artist: '雨笠 将'},
+                        {img: 'storage/images/amagasa_zange.png', title: '雨笠の懺悔', artist: '雨笠 将'},
+                        {img: 'storage/images/amagasa_zange.png', title: '雨笠の懺悔', artist: '雨笠 将'},
+                    ]
+                },
+                {
+                    mainTitle: '特集2',
+                    items: [
+                        {img: 'storage/images/amagasa_mother.png', title: '雨笠の母', artist: '雨笠 将'},
+                        {img: 'storage/images/amagasa_mother.png', title: '雨笠の母', artist: '雨笠 将'},
+                        {img: 'storage/images/amagasa_mother.png', title: '雨笠の母', artist: '雨笠 将'},
+                        {img: 'storage/images/amagasa_mother.png', title: '雨笠の母', artist: '雨笠 将'},
+                        {img: 'storage/images/amagasa_mother.png', title: '雨笠の母', artist: '雨笠 将'},
+                        {img: 'storage/images/amagasa_mother.png', title: '雨笠の母', artist: '雨笠 将'},
+                    ]
+                }
             ]
         }
     }
